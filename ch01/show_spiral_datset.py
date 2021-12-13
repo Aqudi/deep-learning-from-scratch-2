fd166e6778b32c999a7260c5f24b4d46a9186cb8
Aqudi <gjdigj145@gmail.com>
2021-12-13 17:38:57 +0900

    faet: ch01 신경망 복습

---
+++ ch01/show_spiral_datset.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import sys
+
+sys.path.append("..")
+import matplotlib.pyplot as plt
+from dataset import spiral
+
+x, t = spiral.load_data()
+print("x", x.shape)  # (300, 2)
+print("t", t.shape)  # (300, 3)
+
+# plot
+N = 100
+CLS_NUM = 3
+markers = ["o", "x", "^"]
+for i in range(CLS_NUM):
+    plt.scatter(
+        x[i * N : (i + 1) * N, 0],
+        x[i * N : (i + 1) * N, 1],
+        s=40,
+        marker=markers[i],
+    )
+plt.show()
