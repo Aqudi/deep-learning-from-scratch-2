3c10bd351864f28c7b25b4b59f26b41888d6100f
Aqudi <gjdigj145@gmail.com>
2021-12-17 15:31:43 +0900

    fix: contexts와 target 분류하는 range 수정

--- common/util.py
+++ common/util.py
@@ -169,7 +169,7 @@ def create_contexts_target(corpus: np.ndarray, window_size=1):
     target = corpus[window_size:-window_size]
     contexts = []
 
-    for idx in range(window_size, len(corpus - window_size)):
+    for idx in range(window_size, len(corpus) - window_size):
         cs = []
         for t in range(-window_size, window_size + 1):
             if t == 0:
